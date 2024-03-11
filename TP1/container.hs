f7736613b5b2bf6322c6c243e06a63397ad40c80
belubeluga <belugotz@gmail.com>
2024-03-11 19:52:40 -0300

    11/3

---
+++ TP1/container.hs
@@ -0,0 +1,13 @@
+module Container ( Container, newC, destinationC, netC )
+ where
+
+data Container = Con String Int deriving (Eq, Show)-- (Eq, Show) para q use el igual y el show pre establecido        -- (todo desps del = == constructor)
+                    -- String == su destino
+newC :: String -> Int -> Container   -- construye un Contenedor dada una ciudad de destino y un peso en toneladas
+newC destino num = Con destino num --COMO CONSTRUIR UN NUEVO CONTAINER
+
+destinationC :: Container -> String  -- responde la ciuda destino del contenedor
+destinationC (Con destino num) = destino --(?)
+
+netC :: Container -> Int             -- responde el peso en toneladas del contenedor
+netC (Con destino num) = num
---
+++ TP1/route.hs
@@ -0,0 +1,8 @@
+module Route ( Route, newR, inOrderR )
+    where
+
+data Route = Rou [ String ] deriving (Eq, Show)
+
+newR :: [ String ] -> Route                    -- construye una ruta segun una lista de ciudades
+
+inOrderR :: Route -> String -> String -> Bool  -- indica si la primer ciudad consultada esta antes que la segunda ciudad en la ruta
