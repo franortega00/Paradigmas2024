77d379ca8af6ab1ef3342262d17636a034a41b43
belubeluga <belugotz@gmail.com>
2024-03-12 15:48:59 -0300

    primeras pruebas

--- TP1/route.hs
+++ TP1/route.hs
@@ -4,5 +4,7 @@ module Route ( Route, newR, inOrderR )
 data Route = Rou [ String ] deriving (Eq, Show)
 
 newR :: [ String ] -> Route                    -- construye una ruta segun una lista de ciudades
+newR listaDestinos = Rou listaDestinos
 
-inOrderR :: Route -> String -> String -> Bool  -- indica si la primer ciudad consultada esta antes que la segunda ciudad en la ruta
+-- inOrderR :: Route -> String -> String -> Bool  -- indica si la primer ciudad consultada esta antes que la segunda ciudad en la ruta
+-- inOrderR ruta destino1 destino2 = 
---
+++ TP1/testing.hs
@@ -0,0 +1,12 @@
+import Container
+import Route
+import Stack
+import Vessel
+
+cont = newC "MDQ" 5
+barco = cargarV barco cont
+otroBarco = cargarV barco cont
+dobleBarco = cargarV otroBarco cont
+
+test = [ netV otroBarco == 5,
+         netV dobleBarco == 10]
