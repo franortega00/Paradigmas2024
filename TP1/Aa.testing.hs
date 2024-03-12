9adecfe9f536ec867499b50fc4c0cfe75d6706c0
belubeluga <belugotz@gmail.com>
2024-03-12 16:23:37 -0300

    prueba commit

--- TP1/Aa.testing.hs
+++ TP1/Aa.testing.hs
@@ -3,7 +3,22 @@ import Container
 import Stack
 import Vessel-}
 
+{-
+data Container = Con String Int deriving (Eq, Show)-- (Eq, Show) para q use el igual y el show pre establecido        -- (todo desps del = == constructor)
+                    -- String == su destino
+newC :: String -> Int -> Container   -- construye un Contenedor dada una ciudad de destino y un peso en toneladas
+newC destino num = contenedor --COMO CONSTRUIR UN NUEVO CONTAINER
+    where contenedor = Con destino num
+    
+destinationC :: Container -> String  -- responde la ciuda destino del contenedor
+destinationC (Con destino num) = destino --(?)
+
+netC :: Container -> Int             -- responde el peso en toneladas del contenedor
+netC (Con destino num) = num
+-}
 cont = newC "MDQ" 5
+destino = destinationC cont
+peso = netC cont
 {-barco = cargarV barco cont
 otroBarco = cargarV barco cont
 dobleBarco = cargarV otroBarco cont
