ca5ab5917a4faa4c5b7bb2b6cc819ad1a1ed6481
belubeluga <belugotz@gmail.com>
2024-03-12 16:31:13 -0300

    organizacion y testing de funciones Container

--- TP1/Aa.testing.hs
+++ TP1/Aa.testing.hs
@@ -4,21 +4,17 @@ import Stack
 import Vessel-}
 
 {-
-data Container = Con String Int deriving (Eq, Show)-- (Eq, Show) para q use el igual y el show pre establecido        -- (todo desps del = == constructor)
-                    -- String == su destino
-newC :: String -> Int -> Container   -- construye un Contenedor dada una ciudad de destino y un peso en toneladas
-newC destino num = contenedor --COMO CONSTRUIR UN NUEVO CONTAINER
-    where contenedor = Con destino num
-    
-destinationC :: Container -> String  -- responde la ciuda destino del contenedor
-destinationC (Con destino num) = destino --(?)
-
-netC :: Container -> Int             -- responde el peso en toneladas del contenedor
-netC (Con destino num) = num
+FUNCIONES A HACER:
+    Stack --> newS, freeCellsS, stackS, netS, holdsS, popS
+    Vessel --> newV, freeCellsV, loadV, unloadV, netV
+FUNCIONES A PROBAR:    
+    Container --> newC, destinationC, netC 
+    Route --> newR
 -}
 cont = newC "MDQ" 5
 destino = destinationC cont
 peso = netC cont
+testContainer = [destino == "MDQ", peso == 5]
 {-barco = cargarV barco cont
 otroBarco = cargarV barco cont
 dobleBarco = cargarV otroBarco cont
