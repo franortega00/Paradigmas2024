7d48aea6de161cd6931a38db39e0c6574d48eb77
belubeluga <belugotz@gmail.com>
2024-03-19 15:50:50 -0300

    pruebasTest (criterios)

--- TP1/pruebasTest.hs
+++ TP1/pruebasTest.hs
@@ -3,6 +3,11 @@ import Route
 import Stack
 import Vessel
 
+
+-- creaciones inconsistentes : Raise Error
+-- acciones no permitidas : No se hacen
+
+
 {- VESSEL -}
 -- newV , freeCellsV , loadV, unloadV, netV
 rutaABC = newR ["Armenia", "Brunei", "Comoros"]
@@ -10,10 +15,14 @@ testRoute = [   inOrderR rutaABC "Armenia" "Brunei", -- = True
                 not(inOrderR rutaABC "Comoros" "Brunei"), -- not(False)
                 not(inOrderR rutaABC "Armenia" "Ruta inexistente") {-not(False)-}]
 
-contA = newC "Armenia" 10
-contB = newC "Brunei" 10
-barco = newV 3 1 rutaABC --bahias, alturas
-barcoA = loadV barco contA
-barcoAB = loadV barcoA contB --peso y orden
+contA = newC "Armenia" 1
+contB = newC "Brunei" 3
+barco = newV 1 2 rutaABC --bahias, alturas
+barcoB = loadV barco contB
+barcoBA = loadV barcoB contA --peso y orden
+barcoBAB = loadV barcoBA contB
 
+testVessel = [
+        barcoBA == barcoBAB --como ya se excedio el limite de altura, mantiene igual
+            ]
 
