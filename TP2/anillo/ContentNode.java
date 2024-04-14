3516fc21a436e82e4e1134fecf84ab3a7883c6d4
franortega00 <93559617+franortega00@users.noreply.github.com>
2024-04-14 19:25:40 -0300

    Update ContentNode.java

--- TP2/anillo/ContentNode.java
+++ TP2/anillo/ContentNode.java
@@ -3,6 +3,7 @@ package anillo;
 public abstract class ContentNode {
     abstract public ContentNode next();
     abstract public Object current();
+    public abstract ContentNode previous();
     abstract public ContentNode add(Object cargo);
     abstract public ContentNode remove();
 }
